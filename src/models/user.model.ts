8e6b51b29e6da70581eb1f1680c844046adc765b
Mayer Andres <andres.20.chaves@gmail.com>
2023-12-02 22:47:44 -0500

    Modificaciones en el modelo

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -3,11 +3,11 @@ import { User } from "../interfaces/user.interface";
 
 const userSchema = new Schema<User>(
   {
-    username: { type: String, required: true, unique: true, trim: true },
+    name: { type: String, required: true },
+    username: { type: String, unique: true, trim: true },
     email: { type: String, required: true, unique: true, trim: true },
     password: { type: String, required: true },
-    name: { type: String, required: true },
-    age: { type: Number, required: false },
+    age: { type: Number },
   },
   {
     timestamps: true,
